31fc141789c1cf2f5bdb6e575d200342a68a4351
cosdaman <cosdaguy@gmail.com>
2022-02-17 21:13:05 -0800

    added more

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -32,7 +33,19 @@ const resolvers = {
                 );
             }
             throw new AuthenticationError('You need to be logged in!');
-        }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
     },
 };
 
